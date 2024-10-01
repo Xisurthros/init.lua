e4489ff4d1d37977ec82b7a867fe05e233214576
xisurthros <jacobcoburnhobbs@gmail.com>
2024-09-30 21:55:09 -0500

    removed <enter><enter> keymapping

--- init.lua
+++ init.lua
@@ -75,7 +75,6 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set("i", "<Enter><Enter>", "<Esc>")
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
